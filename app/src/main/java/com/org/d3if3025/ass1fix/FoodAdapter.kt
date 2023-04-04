69482b00a8a1f14abfa6179efd1caa0686f86835
Thessa Roxana <thessaroxana3@gmail.com>
2023-04-04 21:40:23 +0700

    done

--- app/src/main/java/com/org/d3if3025/ass1fix/FoodAdapter.kt
+++ app/src/main/java/com/org/d3if3025/ass1fix/FoodAdapter.kt
@@ -1,17 +1,15 @@
 package com.org.d3if3025.ass1fix
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.org.d3if3025.ass1fix.databinding.ItemBinding
 
 class FoodAdapter(private var foodList: ArrayList<Food>) :
-    RecyclerView.Adapter<FoodAdapter.ViewHolder>(), Filterable {
-
-    class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+    class ViewHolder(private val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(food: Food) {
             Glide.with(binding.root)
                 .load(food.image)
@@ -23,8 +21,6 @@ class FoodAdapter(private var foodList: ArrayList<Food>) :
             }
         }
     }
-
-    var filteredList: ArrayList<Food> = foodList
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
         holder.bind(foodList[position])
@@ -38,37 +34,11 @@ class FoodAdapter(private var foodList: ArrayList<Food>) :
             )
         )
 
-    override fun getItemCount() = filteredList.size
+    override fun getItemCount() = foodList.size
 
-    override fun getFilter(): Filter {
-        return object : Filter() {
-
-            override fun performFiltering(charSequence: CharSequence): FilterResults {
-                val charString = charSequence.toString()
-
-                filteredList = when {
-                    charString.isEmpty() -> foodList
-                    else -> {
-                        val internalFilteredList = arrayListOf<Food>()
-                        for (data in foodList) {
-                            if (data.name.contains(charString, ignoreCase = true)) {
-                                internalFilteredList.add(data)
-                            }
-                        }
-                        internalFilteredList
-                    }
-                }
-
-                val filteredResults = FilterResults()
-                filteredResults.values = filteredList
-                return filteredResults
-            }
-
-            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
-                filteredList = p1?.values as ArrayList<Food>
-            }
-
-        }
+    @SuppressLint("NotifyDataSetChanged")
+    fun filtering(filter: ArrayList<Food>) {
+        foodList = filter
+        notifyDataSetChanged()
     }
-
 }
--- app/src/main/java/com/org/d3if3025/ass1fix/MainActivity.kt
+++ app/src/main/java/com/org/d3if3025/ass1fix/MainActivity.kt
@@ -1,37 +1,38 @@
 package com.org.d3if3025.ass1fix
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.org.d3if3025.ass1fix.databinding.ActivityMainBinding
+import java.util.Locale.filter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private var foodList = arrayListOf<Food>()
+//    private var foodList = arrayListOf<Food>()
     private lateinit var fAdapter: FoodAdapter
-    private val list = arrayListOf<String>()
+    private lateinit var list: ArrayList<Food>
 
+    @SuppressLint("NotifyDataChanged", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        foodList.addAll(initItems())
-        fAdapter = FoodAdapter(foodList)
-
-        for (i in foodList.indices) {
-            list.add(foodList[i].name)
-        }
+        list = ArrayList()
+        list.addAll(initItems())
+        fAdapter = FoodAdapter(list)
 
         with(binding.recyclerView) {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = fAdapter
-            setHasFixedSize(true)
         }
+        fAdapter.run { notifyDataSetChanged() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,31 +40,38 @@ class MainActivity : AppCompatActivity() {
 
         val menuItem = menu.findItem(R.id.search_action)
         val searchView = menuItem.actionView as SearchView
-        performSearch(searchView)
 
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    private fun performSearch(searchView: SearchView) {
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                for (i in list.indices) {
-                    Log.wtf("AAAA", "onQueryTextSubmit: ${list[i]}")
-
-                    if (list[i].contains(query!!)) {
-                        fAdapter.filter.filter(query)
-                    }
-                }
-
+            override fun onQueryTextSubmit(query: String): Boolean {
                 return false
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                fAdapter.filter.filter(newText)
-                return true
+            override fun onQueryTextChange(newText: String): Boolean {
+                filter(newText)
+                return false
             }
         })
+
+        return true
     }
+
+    private fun filter(text: String) {
+        val filteredList = arrayListOf<Food>()
+
+        for (item in list) {
+            if (item.name.lowercase().contains(text.lowercase())){
+                filteredList.add(item)
+            }
+        }
+
+        if (filteredList.isEmpty()) {
+            Toast.makeText(this, "No Data Found!", Toast.LENGTH_SHORT).show()
+        } else {
+            fAdapter.filtering(filteredList)
+        }
+    }
+
+
 
     private fun initItems(): ArrayList<Food> {
         return arrayListOf(
